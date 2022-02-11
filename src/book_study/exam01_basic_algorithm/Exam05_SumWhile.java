3b2931cc45f19f9a53bb923378d290c91594a83f
tjdtls690 <85877080+tjdtls690@users.noreply.github.com>
2022-02-11 22:42:52 +0900

    algorithm

---
+++ src/book_study/exam01_basic_algorithm/Exam05_SumWhile.java
@@ -0,0 +1,20 @@
+package book_study.exam01_basic_algorithm;
+
+public class Exam05_SumWhile {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int i = 1;
+		int g = 0;
+		while(i <= 10) {
+			sum += i;
+			i++;
+			g = i;
+		}
+		System.out.println(sum);
+		System.out.println(10*11/2);
+		System.out.println(g);
+		
+	}
+
+}
